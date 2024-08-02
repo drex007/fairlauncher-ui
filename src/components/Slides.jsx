a0222b4a5e62f894cdf1274b884eea58918f570d
drex007 <vykeoj@gmail.com>
2024-08-02 21:43:21 +0100

    responsiveness

--- src/components/Slides.jsx
+++ src/components/Slides.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
-const Slides = ({ slides, current}) => {
+const Slides = ({ slides, current }) => {
     const [currentSlide, setCurrentSlide] = useState(0)
 
     return (
-        <div className='relative w-full md:w-96 overflow-hidden'>
+        <div className='relative lg:w-full w-3/4  overflow-hidden'>
             <div
                 className='flex transition-transform duration-1000 ease-in-out'
                 style={{ transform: `translateX(-${currentSlide * 100}%)` }}
@@ -17,7 +17,7 @@ const Slides = ({ slides, current}) => {
                             <div className='flex-[0.5] my-3 mr-3 rounded-xl py-2'>
                                 <p className='font-poppins lg:text-[15px] text-[11px] text-primary-yellow'>BigMusk Cats</p>
                                 <p className='font-poppins lg:text-[11px] text-[8px] text-white tracking-wider md:mt-3 mt-1'>Ensuring Transparent, Secure, and Equitable Token Distribution For All.</p>
-                                <button className='py-2 md:px-10 px-4 bg-primary-yellow text-blue-600 font-semibold rounded-3xl md:text-[10px] text-[8px] my-2'>Buy Now</button>
+                                <button className='py-2 md:px-10 px-4 bg-primary-yellow text-blue-600 font-semibold rounded-3xl md:text-[10px] text-[8px] lg:my-3 my-2'>Buy Now</button>
                             </div>
                         </div>
                     </div>
