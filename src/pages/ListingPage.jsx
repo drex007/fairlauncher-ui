724bb3d687075d029c69bc564d346c0d56194e4d
drex007 <vykeoj@gmail.com>
2024-09-11 04:43:12 +0100

    fixes

--- src/pages/ListingPage.jsx
+++ src/pages/ListingPage.jsx
@@ -11,7 +11,7 @@ const ListingPage = () => {
         <div className='w-full h-screen bg-gray-100 min-w-[100%] fixed p-1'>
             <div className=' flex flex-1  h-full'>
                 <div className='hidden lg:block relative flex-[0.25]  bg-cover bg-no-repeat bg-center rounded-2xl mt-0 w-full' style={{ backgroundImage: `url(${bgRocket})` }}>
-                    <img src={rocket} alt="" className='flex min-w-[200%] min-h-[120%] -bottom-40 -left-24  absolute  z-10' />
+                    <img src={rocket} alt="" className='flex max-w-[200%] 2xl:max-w-[185%] 3xl:max-w-[150%] 3xl:max-h-[100%] min-h-[120%] -bottom-40 -left-24  absolute  z-10' />
 
                 </div>
                 {showListingPageTwo === false ? <LisitingPageOne showListingPageTwo={showListingPageTwo} setshowListingPageTwo={setshowListingPageTwo} /> :
