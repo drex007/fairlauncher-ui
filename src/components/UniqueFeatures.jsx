48ce13035d2ce4cd7180bfc8be77cb46adf2b950
drex007 <vykeoj@gmail.com>
2024-11-07 13:47:17 +0100

    feat: responsiveness

--- src/components/UniqueFeatures.jsx
+++ src/components/UniqueFeatures.jsx
@@ -3,7 +3,7 @@ import { light } from '../assets'
 
 const UniqueFeatures = () => {
   return (
-    <div className='lg:flex lg:flex-1 2xl:mt-28  2xl:px-48 lg:px-16 px-8 my-8'>
+    <div className='lg:flex lg:flex-1 2xl:mt-28  2xl:px-48 lg:px-16 px-2 my-8'>
       <div className='flex-[0.5]'>
         <div className='lg:ml-16'>
           <p className='2xl:text-[60px] text-blue-700 lg:text-[40px] font-semibold text-[25px] flex lg:justify-start   justify-center'>Unique Features</p>
@@ -12,7 +12,7 @@ const UniqueFeatures = () => {
         <img src={light} alt="" className='hidden 2xl:w-4/5 lg:block w-4/6' />
 
       </div>
-      <div className='flex-[0.5] flex justify-end space-x-10 items-center'>
+      <div className='flex-[0.5] flex lg:justify-end space-x-10 items-center'>
         <div className='2xl:w-1/3 w-2/5'>
           <div className='flex space-x-2 2xl:my-4 my-2'>
             <p className='2xl:h-[20px] 2xl:w-[40px] h-[10px] w-[25px] bg-primary-yellow mt-4'></p>
